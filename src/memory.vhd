e3a1760cc4ad136f3c7613e71c6b17e4c06c85af
Plypy <jake199436@gmail.com>
2015-03-06 09:11:07 +0800

    update memory with length option

--- src/memory.vhd
+++ src/memory.vhd
@@ -11,6 +11,7 @@ entity memory is
     clk : in std_logic;
     memrd : in std_logic;
     memwr : in std_logic;
+    memlen : in LEN_TYPE;
     addr : in vec32;
     din : in vec32;
     dout : out vec32
@@ -19,23 +20,39 @@ end entity memory;
 
 architecture behav of memory is
 
-  subtype byte is VEC8;
+  subtype byte_type is VEC8;
   -- type mem is array (0 to 4294967295); -- 2^32-1
-  type mem is array (0 to 65535); -- test 16bits address first
+  type mem is array (0 to 65535) of byte_type; -- test 16bits address first
 
   signal sram : mem;
 
 begin
 
-  rw_proc : process( clk, memrd, memwr )
+  rw_proc : process( clk, memrd, memwr, memlen )
     variable adr : integer;
   begin
     if (falling_edge(clk)) then
       adr := to_integer(unsigned(addr));
       if (memrd = '1') then
-        dout <= sram(adr);
+        if (memlen = BYTE) then
+          dout <= x"000000" & sram(adr);
+        elsif (memlen = HWORD) then
+          dout <= x"0000" & sram(adr+1) & sram(adr);
+        else
+          dout <= sram(adr+3) & sram(adr+2) & sram(adr+1) & sram(adr);
+        end if;
       elsif (memwr = '1') then
-        sram(adr) <= din;
+        if (memlen = BYTE) then
+          sram(adr) <= din(7 downto 0);
+        elsif (memlen = HWORD) then
+          sram(adr) <= din(7 downto 0);
+          sram(adr+1) <= din(15 downto 0);
+        else
+          sram(adr) <= din(7 downto 0);
+          sram(adr+1) <= din(15 downto 8);
+          sram(adr+2) <= din(23 downto 16);
+          sram(adr+3) <= din(31 downto 24);
+        end if;
       end if;
     end if;
   end process rw_proc;
