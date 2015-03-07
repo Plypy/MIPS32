df08ed460f5ebfb30191d8cec009050bbdb9624f
Plypy <jake199436@gmail.com>
2015-03-07 16:09:44 +0800

    align addresses

--- src/memory.vhd
+++ src/memory.vhd
@@ -29,29 +29,31 @@ architecture behav of memory is
 begin
 
   rw_proc : process( CLK, MEMRD, MEMWR, MEMLEN )
-    variable adr : integer;
+    variable adr0, adr1, adr2 : integer;
   begin
     if (falling_edge(CLK)) then
-      adr := to_integer(unsigned(ADDR));
+      adr0 := to_integer(unsigned(ADDR));
+      adr1 := to_integer(unsigned(ADDR(31 downto 1) & '0'));
+      adr2 := to_integer(unsigned(ADDR(31 downto 2) & '0' & '0'));
       if (MEMRD = '1') then
         if (MEMLEN = BYTE) then
-          DOUT <= x"000000" & sram(adr);
+          DOUT <= x"000000" & sram(adr0);
         elsif (MEMLEN = HWORD) then
-          DOUT <= x"0000" & sram(adr+1) & sram(adr);
+          DOUT <= x"0000" & sram(adr1+1) & sram(adr1);
         else
-          DOUT <= sram(adr+3) & sram(adr+2) & sram(adr+1) & sram(adr);
+          DOUT <= sram(adr2+3) & sram(adr2+2) & sram(adr2+1) & sram(adr2);
         end if;
       elsif (MEMWR = '1') then
         if (MEMLEN = BYTE) then
-          sram(adr) <= DIN(7 downto 0);
+          sram(adr0) <= DIN(7 downto 0);
         elsif (MEMLEN = HWORD) then
-          sram(adr) <= DIN(7 downto 0);
-          sram(adr+1) <= DIN(15 downto 0);
+          sram(adr1) <= DIN(7 downto 0);
+          sram(adr1+1) <= DIN(15 downto 0);
         else
-          sram(adr) <= din(7 downto 0);
-          sram(adr+1) <= din(15 downto 8);
-          sram(adr+2) <= din(23 downto 16);
-          sram(adr+3) <= din(31 downto 24);
+          sram(adr2) <= din(7 downto 0);
+          sram(adr2+1) <= din(15 downto 8);
+          sram(adr2+2) <= din(23 downto 16);
+          sram(adr2+3) <= din(31 downto 24);
         end if;
       end if;
     end if;
